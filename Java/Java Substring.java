50d13185bd23235bb9f9abccf39c33a087ac60dc
Pawan Kumar N <kumarpawan49995@gmail.com>
2026-02-01 12:28:37 +0530

    Update Java Substring.java

--- Java/Java Substring.java
+++ Java/Java Substring.java
@@ -1,11 +1,17 @@
-
-public class Solution {
+ublic class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
         int start = in.nextInt();
         int end = in.nextInt();
-        System.out.println(S.substring(start,end));
+        //System.out.println(S.substring(start,end));
+        StringBuilder SubStr=new StringBuilder();
+        for(int i=0;i<S.length();i++){
+            if(i>=start && i<end){
+            SubStr.append(S.charAt(i));
+            }
+        }
+        System.out.println(SubStr);
     }
 }
