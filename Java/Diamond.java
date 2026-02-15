4137f8f7257af7585b9efc197392211b8503f119
Pawan Kumar <kumarpawan49995@gmail.com>
2026-02-15 18:49:35 +0530

    Create Diamond.java

---
+++ Java/Diamond.java
@@ -0,0 +1,26 @@
+
+class Diamond{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+             System.out.print(" ");
+            }
+            int star=2*i-1;
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+       for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+             System.out.print(" ");
+            }
+            int star=2*i-1;
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
